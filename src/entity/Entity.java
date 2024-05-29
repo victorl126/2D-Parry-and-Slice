fd3f612d0a1f07a71aeea5fec3a5f3244d40d0d2
victorl126 <victorl126@nycstudents.net>
2024-05-29 10:20:15 -0400

    a

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -7,7 +7,9 @@ public class Entity
     public int x, y;
     public int speed;
 
-    public BufferedImage attack, idle, run_r, run_l;
+    public BufferedImage attack, idle, run_r1, run_r2, run_r3, run_r4, run_r5, run_r6, run_r7, run_r8, run_l1, run_l2, run_l3, run_l4, run_l5, run_l6, run_l7, run_l8, guard1, guard2, guard3, guard4, guard5, guard6;
     public String direction;
 
+    public int spriteCounter = 0;
+    public int spriteNum = 1;
 }
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -37,8 +37,28 @@ public class Player extends Entity
             {
                 attack = ImageIO.read(getClass().getResourceAsStream("/player/ATTACK 1.png"));
                 idle = ImageIO.read(getClass().getResourceAsStream("/player/IDLE.png"));
-                run_r = ImageIO.read(getClass().getResourceAsStream("/player/RUN.png"));
-                run_l = ImageIO.read(getClass().getResourceAsStream("/player/RUN_LEFT.png"));
+                run_r1 = ImageIO.read(getClass().getResourceAsStream("/player/run_right_1.png"));
+                run_r2 = ImageIO.read(getClass().getResourceAsStream("/player/run_right_2.png"));
+                run_r3 = ImageIO.read(getClass().getResourceAsStream("/player/run_right_3.png"));
+                run_r4 = ImageIO.read(getClass().getResourceAsStream("/player/run_right_4.png"));
+                run_r5 = ImageIO.read(getClass().getResourceAsStream("/player/run_right_5.png"));
+                run_r6 = ImageIO.read(getClass().getResourceAsStream("/player/run_right_6.png"));
+                run_r7 = ImageIO.read(getClass().getResourceAsStream("/player/run_right_7.png"));
+                run_r8 = ImageIO.read(getClass().getResourceAsStream("/player/run_right_8.png"));
+                run_l1 = ImageIO.read(getClass().getResourceAsStream("/player/run_l1.png"));
+                run_l2 = ImageIO.read(getClass().getResourceAsStream("/player/run_l2.png"));
+                run_l3 = ImageIO.read(getClass().getResourceAsStream("/player/run_l3.png"));
+                run_l4 = ImageIO.read(getClass().getResourceAsStream("/player/run_l4.png"));
+                run_l5 = ImageIO.read(getClass().getResourceAsStream("/player/run_l5.png"));
+                run_l6 = ImageIO.read(getClass().getResourceAsStream("/player/run_l6.png"));
+                run_l7 = ImageIO.read(getClass().getResourceAsStream("/player/run_l7.png"));
+                run_l8 = ImageIO.read(getClass().getResourceAsStream("/player/run_l8.png"));
+                guard1 = ImageIO.read(getClass().getResourceAsStream("/player/guard1.png"));
+                guard2 = ImageIO.read(getClass().getResourceAsStream("/player/guard2.png"));
+                guard3 = ImageIO.read(getClass().getResourceAsStream("/player/guard3.png"));
+                guard4 = ImageIO.read(getClass().getResourceAsStream("/player/guard4.png"));
+                guard5 = ImageIO.read(getClass().getResourceAsStream("/player/guard5.png"));
+                guard6 = ImageIO.read(getClass().getResourceAsStream("/player/guard1.png"));
 
             }catch(IOException e)
             {
@@ -58,9 +78,43 @@ public class Player extends Entity
                 direction = "right";
                 x += speed;
             }
-            else
+
+            spriteCounter++;
+            if(spriteCounter > 10)
             {
-                direction = null;
+                if (spriteNum == 1)
+                {
+                    spriteNum = 2;
+                }
+                else if (spriteNum == 2)
+                {
+                    spriteNum = 3;
+                }
+                else if (spriteNum == 3)
+                {
+                    spriteNum = 4;
+                }
+                else if (spriteNum == 4)
+                {
+                    spriteNum = 5;
+                }
+                else if (spriteNum == 5)
+                {
+                    spriteNum = 6;
+                }
+                else if (spriteNum == 6)
+                {
+                    spriteNum = 7;
+                }
+                else if (spriteNum == 7)
+                {
+                    spriteNum = 8;
+                }
+                else if (spriteNum == 8)
+                {
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
             }
         }
 
@@ -74,14 +128,73 @@ public class Player extends Entity
 
             switch(direction)
             {
-                case null:
-                    image = idle;
-                    break;
                 case "right":
-                    image = run_r;
+                    if (spriteNum == 1)
+                    {
+                        image = run_r1;
+                    }
+                    if (spriteNum == 2)
+                    {
+                        image = run_r2;
+                    }
+                    if (spriteNum == 3)
+                    {
+                        image = run_r3;
+                    }
+                    if (spriteNum == 4)
+                    {
+                        image = run_r4;
+                    }
+                    if (spriteNum == 5)
+                    {
+                        image = run_r5;
+                    }
+                    if (spriteNum == 6)
+                    {
+                        image = run_r6;
+                    }
+                    if (spriteNum == 7)
+                    {
+                        image = run_r7;
+                    }
+                    if (spriteNum == 8)
+                    {
+                        image = run_r8;
+                    }
                     break;
                 case "left":
-                    image = run_l;
+                    if (spriteNum == 1)
+                    {
+                        image = run_l1;
+                    }
+                    if (spriteNum == 2)
+                    {
+                        image = run_l2;
+                    }
+                    if (spriteNum == 3)
+                    {
+                        image = run_l3;
+                    }
+                    if (spriteNum == 4)
+                    {
+                        image = run_l4;
+                    }
+                    if (spriteNum == 5)
+                    {
+                        image = run_l5;
+                    }
+                    if (spriteNum == 6)
+                    {
+                        image = run_l6;
+                    }
+                    if (spriteNum == 7)
+                    {
+                        image = run_l7;
+                    }
+                    if (spriteNum == 8)
+                    {
+                        image = run_l8;
+                    }
                     break;
             }
             g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
